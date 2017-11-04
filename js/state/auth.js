081276e580ff7ed219764682f2fee9a53199cf7a
Vaclav Klecanda <vencax77@gmail.com>
2017-11-04 17:19:34 +0100

    fix

--- js/state/auth.js
+++ js/state/auth.js
@@ -2,7 +2,7 @@ import { observable, computed, toJS, action, transaction, extendObservable, asMa
 import CommentsStateInit from 'fb-like-discussions/state'
 
 const LSTORAGE_USER_KEY = 'opendemocracy_user'
-const LSTORAGE_TOKEN_KEY = 'opendemocracy_user'
+const LSTORAGE_TOKEN_KEY = 'opendemocracy_token'
 
 class AuthStore {
 
@@ -35,6 +35,7 @@ class AuthStore {
   @action logout() {
     this.loggedUser = null
     this.token = null
+    this.goTo('login')
   }
 
   @action handleLoginFormChange(attr, val) {
@@ -55,6 +56,7 @@ class AuthStore {
       localStorage.setItem(LSTORAGE_USER_KEY, JSON.stringify(res.data.user))
       this.token = res.data.token
       localStorage.setItem(LSTORAGE_TOKEN_KEY, JSON.stringify(res.data.token))
+      this.goTo('dashboard')
     })
     .catch((err) => {
       this.error = err
