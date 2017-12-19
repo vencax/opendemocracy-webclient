5348e3aa652b8a6779a8fb709efd2cda2dad6fbb
vencax <vencax@centrum.cz>
2017-12-19 12:41:39 +0100

    auth pages moved to separate folder

--- js/components/pages/auth/register.js
+++ js/components/pages/auth/register.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { observer, inject } from 'mobx-react'
-import {__} from '../../state/i18n'
+import {__} from '../../../state/i18n'
 
 const RegisterView = ({store, afterLogin}) => {
   //
